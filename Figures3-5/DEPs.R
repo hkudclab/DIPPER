e9b1c8a771d73b4f5ac24ef56bc8176437561f4d
hkudclab <63039388+hkudclab@users.noreply.github.com>
2020-04-28 12:03:54 +0800

    Update DEPs.R

--- Figures3-5/DEPs.R
+++ Figures3-5/DEPs.R
@@ -1,11 +1,15 @@
 agegrp<-"young"
 OUTDIR<-"DEGs-for-young-DEC5"
+outPDF<-"DEG.young.only.DEC4.pdf"
 source("step5.DEGs.within.young.or.old.only.R")
 
+##########################
 agegrp<-"old"
 OUTDIR<-"DEGs-for-old-DEC5"
+outPDF<-"DEG.old.only.DEC4.pdf"
 source("step5.DEGs.within.young.or.old.only.R")
 
+##########################
 
 source("step7.DEGs.young.vs.old.R")
 
